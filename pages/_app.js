498aa8df9598e58ba034c3fed613cc1bdcfac912
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-08-29 12:54:55 +0700

    Add styles config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
+import '@fontsource/lora';
+import '@fontsource/lato';
+import '@fontsource/playfair-display';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from '../styles/ChakraConfig';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ styles/ChakraConfig.js
@@ -0,0 +1,82 @@
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const styles = {
+  global: {
+    // styles for the `body`
+    body: {
+      fontFamily: 'Lora'
+    },
+    '*': {
+      margin: 0,
+      padding: 0
+    }
+  }
+};
+
+const breakpoints = createBreakpoints({
+  sm: '376px',
+  semiMd: '480px',
+  md: '754px',
+  lg: '944px',
+  xl: '1024px'
+});
+
+// 2. Extend the theme to include custom colors, fonts, etc
+export const fonts = {
+  playfairDisplay: 'Playfair Display',
+  lora: 'Lora',
+  lato: 'Lato'
+};
+
+export const fontSizes = {
+  heading: '36px',
+  subheading1: '24px',
+  subheading2: '18px',
+  subheading3: '16px',
+  paragraph: '14px',
+  smallText: '10px'
+};
+
+export const colors = {
+  primary: {
+    brown1: '#BF9A69',
+    brown2: '#BFA27C',
+    lightTomatoScheme: {
+      100: '#FECACA',
+      800: '#7F1D1D'
+    },
+    limeScheme: {
+      100: '#D9F99D',
+      800: '#166534'
+    },
+    yellowScheme: {
+      100: '#F0E68F',
+      800: '#854D0E'
+    }
+  },
+  secondary: {
+    cream1: '#F6CFA1',
+    cream2: '#F6DAB8'
+  },
+  notification: {
+    green: '#3BCA5A',
+    red: '#CA473B'
+  },
+  neutral: {
+    black: '#3F3F3F',
+    grey: '#878787',
+    bg: '#F7F8F9',
+    white: '#FFFFFF'
+  }
+};
+
+const theme = extendTheme({
+  breakpoints,
+  styles,
+  colors,
+  fonts,
+  fontSizes
+});
+
+export default theme;
