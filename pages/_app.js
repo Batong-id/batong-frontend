59ba6301530411bea6f4fd13838ab018fa2e53f5
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-09-03 23:20:20 +0700

    Add more fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '../styles/globals.css';
 import '@fontsource/lora';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
+import '@fontsource/playfair-display/400.css';
+import '@fontsource/playfair-display/700.css';
 import '@fontsource/playfair-display/900.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
